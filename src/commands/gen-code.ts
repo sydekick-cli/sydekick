acfb1596e0c97daa8126227a9f5c2f7fc36ff4f8
Seth Lessard <sethlessard@outlook.com>
2023-04-03 17:14:42 -0500

    fix(sidekick gen-code): force question handling to be deferred to the user

--- src/commands/gen-code.ts
+++ src/commands/gen-code.ts
@@ -1,8 +1,69 @@
-import path from "path";
+import path, { basename, dirname, resolve } from "path";
 import { ChatSession } from "../ChatSession";
 import { Prompt } from "../Prompt";
 import { readFile, writeFile } from "fs/promises";
 import { existsSync, mkdirSync } from "fs";
+
+export const FORMAT_DESCRIPTION = `
+  <new files>
+  <non-editable reference files>
+  <editable reference files>
+  <commands to run>
+  <notes>
+
+  where <new files> is the following format:
+      New files:
+      # new-file 1 relative path
+      \`\`\`
+      new-file 1 contents
+      \`\`\`
+      # new-file 2 relative path
+      \`\`\`
+      new-file 2 contents
+      \`\`\`
+      ...
+
+  where <non-editable reference files> is the following format:
+      Non-editable reference files:
+      # non-editable-file 1 relative path
+      \`\`\`
+      non-editable-file 1 contents
+      \`\`\`
+      # non-editable-file 2 relative path
+      \`\`\`
+      non-editable-file 2 contents
+      \`\`\`
+      ...
+
+  where <editable reference files> is the following format:
+      Editable reference files:
+      # editable-file 1 relative path
+      \`\`\`
+      editable-file 1 contents
+      \`\`\`
+      # editable-file 2 relative path
+      \`\`\`
+      editable-file 2 contents
+      \`\`\`
+      ...
+
+  where <commands to run> is the following format:
+      Commands to run:
+      # comment explaining command 1
+      command 1
+      # comment explaining command 2
+      command 2
+      ...
+
+  and <notes> is the following format:
+      Notes:
+      # note 1
+      # note 2
+      ...
+
+  If the user did not specify "editable reference files", you should omit the <editable reference files> section in favor of the <new files> section in your response.
+  Finally, if the user did not specify "non-editable reference files", you should omit the <non-editable reference files> section from your response.
+`;
 
 export const SYSTEM_PROGRAMMING_PROMPT = `
 You are a helpful programmer who wants to help the user to solve a problem by writing a program.
@@ -10,77 +71,17 @@ In a moment, the user will describe the problem they are trying to solve.
 The user may also ask you to write the program in a specific language. If they do not specify a language, you may choose one.
 The user may also append existing code to their question. You should reference this code and ensure that your code works with it. Some of these files will be known as "editable reference files". You may edit these "editable reference files" to help you solve the problem. There are also "non-editable reference files". You may also reference "non-editable reference files", but you may not edit them.
 If the user does append existing code, you should first attempt to solve the problem only by updating the "editable reference files" first and only writing new code files when it would not make sense to include the functionality in the "editable reference files". 
-The user may also specify a destination file. If they do, you attempt to make all code changes in that file, but you may also fallback to "editable reference files" if necessary.
-You are allowed to use any resources you would like to solve the problem. You may use the internet, books, or any other resources you would like. You are also allow to ask the user questions if you need more information to solve the problem.
+If the user does not append existing code you can assume that you will need to write all the code to solve the problem.
+The user may also specify a destination file. If they do, you attempt to make all code changes in that file, but you may also fallback to "editable reference files" if necessary. If the user does not specify a destination file, you may choose one.
+You are allowed to use any resources you would like to solve the problem. You may use the internet, books, or any other resources you would like. You are also allow to ask the user questions if you need more information to solve the problem, however it is encouraged that you do your best to solve the problem without asking the user questions.
 You must also ensure that your code is not breaching any copyright laws or licenses. This is a must.
 You are also strongly encouraged to comment your code to explain your thought process and how you solved the problem. This is helpful for the user to understand how you solved the problem and to learn from your code.
 If the language you are using is strongly typed, you should also ensure that your code compiles and is free of type errors. You should also ensure to provide as much type information as possible.
 If you are able to write tests for your code, you should do so and include them in your response. You should also ensure that your tests pass.
-
-If you need more information from the user or if you need to ask the user a question, do so in the following format:
-Q: <question>
-
-where <question> is the question you would like to ask the user. You must follow this format for asking questions exactly, otherwise the user will not understand that you are asking a question.
+If you need to create new directories or files to solve the problem, you should do so.
 
 If you have all the information you need to solve the problem, you should write the code to solve the problem and respond with the following format described by the sections below:
-    <new files>
-    <non-editable reference files>
-    <editable reference files>
-    <commands to run>
-    <notes>
-
-    where <new files> is the following format:
-        New files:
-        # new-file 1 relative path
-        \`\`\`
-        new-file 1 contents
-        \`\`\`
-        # new-file 2 relative path
-        \`\`\`
-        new-file 2 contents
-        \`\`\`
-        ...
-
-    where <non-editable reference files> is the following format:
-        Non-editable reference files:
-        # non-editable-file 1 relative path
-        \`\`\`
-        non-editable-file 1 contents
-        \`\`\`
-        # non-editable-file 2 relative path
-        \`\`\`
-        non-editable-file 2 contents
-        \`\`\`
-        ...
-
-    where <editable reference files> is the following format:
-        Editable reference files:
-        # editable-file 1 relative path
-        \`\`\`
-        editable-file 1 contents
-        \`\`\`
-        # editable-file 2 relative path
-        \`\`\`
-        editable-file 2 contents
-        \`\`\`
-        ...
-
-    where <commands to run> is the following format:
-        Commands to run:
-        # comment explaining command 1
-        command 1
-        # comment explaining command 2
-        command 2
-        ...
-
-    and <notes> is the following format:
-        Notes:
-        # note 1
-        # note 2
-        ...
-
-    If the user did not specify "editable reference files", you should omit the <editable reference files> section in favor of the <new files> section in your response.
-    Finally, if the user did not specify "non-editable reference files", you should omit the <non-editable reference files> section from your response.
+${FORMAT_DESCRIPTION}
 
 If you are unable to solve the problem, you should respond with the following format:
     Sorry, I am not sure how to solve this problem.
@@ -137,24 +138,40 @@ export async function genCode(options: GenCodeOptions) {
   let finished = false;
   while (!finished) {
     const response = await chatSession.executeSession();
-    if (response.data.choices[0].message?.content.toLowerCase().startsWith("q:")) {
-      const question = response.data.choices[0].message?.content.slice(2).trim();
-      console.log(question);
-      const uResponse = await userResponse(" > ");
-      chatSession.chatAsUser(uResponse);
-    } else if (response.data.choices[0].message?.content.toLowerCase().startsWith("sorry")) {
+    const responseContent = response.data.choices[0].message?.content;
+    if (!responseContent) {
+      console.error("No response content.");
+      process.exit(1);
+    }
+    if (responseContent.toLowerCase().startsWith("sorry")) {
       console.error(response.data.choices[0].message?.content);
       process.exit(1);
     } else {
-      const responseContent = response.data.choices[0].message?.content;
       console.log(responseContent);
-      if (!responseContent) {
-        console.error("No response content.");
-        process.exit(1);
-      }
+      chatSession.chatAsSystem(responseContent);
       console.log("\n\n");
 
       const parsedResponse = parseResponse(responseContent);
+      if (!parsedResponse) {
+        const isQuestion =
+          responseContent.endsWith("?") ||
+          (await Prompt.getRequiredInput("Did gpt prompt you with a question? (y/n) > ")) === "y";
+        if (isQuestion) {
+          const userResponse = await Prompt.getRequiredInput("What was your response? > ");
+          chatSession.chatAsUser(userResponse);
+          continue;
+        }
+        const response = await Prompt.getRequiredInput(
+          "Failed to parse the response. Ask gpt to retry? (y/n) > "
+        );
+        if (response === "n") {
+          process.exit(1);
+        }
+        chatSession.chatAsUser(
+          `I was unable to parse your answer. Please make sure it follows the format initially described. Here it is again if you forgot:\n${FORMAT_DESCRIPTION}`
+        );
+        continue;
+      }
       await acceptOrRejectResponse(parsedResponse, directory);
 
       finished = true;
@@ -203,12 +220,13 @@ async function acceptOrRejectFiles(
       continue;
     }
 
-    if (directory && !existsSync(directory)) {
+    const baseDir = dirname(resolvedPath);
+    if (!existsSync(baseDir)) {
       // create the directory
       try {
-        mkdirSync(directory, { recursive: true });
+        mkdirSync(basename(baseDir), { recursive: true });
       } catch (error) {
-        console.error(`Failed to create directory '${directory}'.`);
+        console.error(`Failed to create directory '${baseDir}'.`);
         console.error(error);
         continue;
       }
@@ -250,7 +268,7 @@ interface ParsedResponse {
   notes: string[];
 }
 
-function parseResponse(responseContent: string) {
+function parseResponse(responseContent: string): ParsedResponse | undefined {
   const responseContentSplit = responseContent.split("\n");
   const indexNewFiles = responseContentSplit.indexOf("New files:");
   const indexNonEditableFiles = responseContentSplit.indexOf("Non-editable reference files:");
@@ -323,6 +341,12 @@ function parseResponse(responseContent: string) {
     commands: [],
     notes: [],
   };
+
+  // if no files were parsed then fail because gpt did not respond in the expected format
+  if (indexNewFiles < 0 && indexNonEditableFiles < 0 && indexEditableFiles < 0) {
+    console.error("Failed to parse response from gpt.");
+    return undefined;
+  }
 
   if (indexNewFiles >= 0) {
     const endIndex =
