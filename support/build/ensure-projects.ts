978976f8ba2d2ece7725bfd679f8233ef73e4601
Seth Lessard <sethlessard@outlook.com>
2023-04-08 00:10:31 -0500

    chore: ensure required package.json fields are set, node16 types

--- support/build/ensure-projects.ts
+++ support/build/ensure-projects.ts
@@ -31,10 +31,34 @@ void (async () => {
     debug(
       `Processing ${tsconfigProject.name} (tsconfig.json: ${tsconfigProject.tsConfigPath}, package.json: ${tsconfigProject.packageJsonPath})`
     );
+    // ensure the package.json is targeting node16
+    tsconfigProject.packageJson.engines = tsconfigProject.packageJson.engines || {};
+    if (!tsconfigProject.packageJson.engines.node) {
+      tsconfigProject.packageJson.engines.node = ">=16";
+    }
     // ensure the package.json is of type module
     tsconfigProject.packageJson.type = tsconfigProject.packageJson.type || "module";
     // ensure the package.json has a main entry point
     tsconfigProject.packageJson.main = tsconfigProject.packageJson.main || "dist/index.js";
+    // ensure the package.json has a types entry point
+    tsconfigProject.packageJson.types = tsconfigProject.packageJson.types || "dist/index.d.ts";
+    // ensure the homepage is set
+    tsconfigProject.packageJson.homepage =
+      tsconfigProject.packageJson.homepage || "https://github.com/feedmefries/sydekick";
+    // ensure the repository is set
+    tsconfigProject.packageJson.repository = tsconfigProject.packageJson.repository || {
+      type: "git",
+      url: "git+https://github.com/feedmefries/sydekick.git",
+    };
+    // ensure the bugs url is set
+    tsconfigProject.packageJson.bugs = tsconfigProject.packageJson.bugs || {
+      url: "https://github.com/feedmefries/sydekick/issues",
+    };
+    // ensure the license is set
+    tsconfigProject.packageJson.license = tsconfigProject.packageJson.license || "MIT";
+    // ensure the author is set
+    tsconfigProject.packageJson.author =
+      tsconfigProject.packageJson.author || "Seth Lessard <sethlessard@outlook.com>";
     ensureBaseTSConfigHasReferenceToThisProject(tsconfigProject, rootTsconfigProject);
     // ensure tsconfig.json extends tsconfig.base.json
     ensureTSConfigExtendsRootBaseTSConfig(tsconfigProject, rootTsConfigBasePath);
